3a666d93374f951146395ee533ccf640da4937ba
Prinn Prinyanut <prinnprinyanut@gmail.com>
2020-08-18 10:51:04 -0600

    Updates

---
+++ precache-manifest.0ca9614067149df9eb7e2bfd9a1c160a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66096e40252a9ca35266a5907d16acc6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "423c321dde7765743a0e",
+    "url": "/monster-rolodex/static/css/main.908b6363.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423c321dde7765743a0e",
+    "url": "/monster-rolodex/static/js/main.5d715942.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
